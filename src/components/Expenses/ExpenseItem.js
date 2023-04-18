7db6e49745d4dcef24f8bdac25f43efea3fa9fb4
Alix Wawa <alixwawa@yahoo.com>
2023-04-17 22:19:13 -0500

    added more structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 	return (
-		<Card className='expense-item'>
+		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
